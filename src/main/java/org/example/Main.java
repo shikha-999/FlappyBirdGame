20ca56110ad455b4223d8d38e498a2386b670f71
shikha-999 <yadav.shikha.ys@gmail.com>
2024-06-28 01:30:09 +0530

    Flappy Bird Game V1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,35 @@
 package org.example;
+import javax.swing.*;
+//import org.example.FlappyBird;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int boardWidth = 360;
+        int boardHeight = 640;
+
+        /*JFrame is a top level container (window)
+        *To create GUI with java Swing, we need container, here JFrame
+        *All GUI apps require JFrame
+        */
+        JFrame frame = new JFrame("Flappy Bird");
+        frame.setSize(boardWidth, boardHeight);
+
+        /*to position the JFrame in the center of the screen when the application starts.
+        *In Java Swing, the setLocationRelativeTo(Component c) method is used to set the location of the window relative to the specified component.
+        *If the component is null, or if the specified component is not currently showing, then the window is placed in the center of the screen.
+        */
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+
+        //terminate the app when frame is closed
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        FlappyBird flappyBird = new FlappyBird();
+        frame.add(flappyBird);
+        frame.pack();
+        flappyBird.requestFocus();
+        frame.setVisible(true); //visibility true only after setting Jpanel to our frame
+
     }
 }
